dda91d81ffb9ba4a16a8bc522526b1c1fba55296
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 00:58:45 -0600

    update

--- index.php
+++ index.php
@@ -28,6 +28,69 @@
     <a href=""><button class="btn btn-primary">Record Incident</button></a>
 </div>
 
+<div class="group-card-decks">
+  <div class="card-deck">
+  <div class="card" style="width: 18rem;">
+      <!-- <img src="images/ss9.png" class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <br>
+        <h5 class="card-title" style="text-align: center;">QR Code Sheet</h5>
+        <div class="col-md-12 text-center">
+        <a href="docs/Emergency Prep QRC.pdf" class="btn btn-primary" download>Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+      <!-- <img src="images/ss10.png" class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <br>
+        <h5 class="card-title" style="text-align: center;">Area Preparedness Guide</h5>
+        <div class="col-md-12 text-center">
+        <a href="docs/Temporal-Preparedness-Guide-NASE-Area-Guide.pdf" class="btn btn-primary" download>Download</a>
+      </div>
+      </div>
+    </div>
+     <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center;">Are You Ready? <br> Quick Quiz</h5>
+        <div class="col-md-12 text-center">
+        <a href="https://PollEv.com/beaufortrs050" class="btn btn-primary" target="_blank">Click Here</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+<!-- Row 2 -->
+<div class="card-deck">
+<div class="card" style="width: 18rem;">
+    <img src="images/ss1.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title" style="text-align: center;">Evacuation Routes</h5>
+      <div class="col-md-12 text-center">
+      <a href="https://hurricane.sc/evacuate" class="btn btn-primary" target="_blank">Learn More</a>
+      </div>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="images/ss2.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title" style="text-align: center;">Hurricane Tracker</h5>
+      <div class="col-md-12 text-center">
+      <a href="https://www.nhc.noaa.gov/" class="btn btn-primary" target="_blank">Learn More</a>
+      </div>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="images/ss3.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title" style="text-align: center;">Beaufort County Re-Entry</h5>
+      <div class="col-md-12 text-center">
+      <a href="https://www.beaufortcountysc.gov/" class="btn btn-primary" target="_blank">Learn More</a>
+    </div>
+    </div>
+  </div>
+</div>
+
 
 
 </body>
