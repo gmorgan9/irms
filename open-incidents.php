f38d490d117d72b4a53642253721a28c276d1f12
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 10:50:00 -0600

    update

--- open-incidents.php
+++ open-incidents.php
@@ -36,7 +36,7 @@
     <div class="card-body d-flex flex-column align-items-center">
         <div class="card-body">
             <div class="d-flex justify-content-center">
-                <h1 style="font-size: 100px;">2</h1> <br>
+                <h1 style="font-size: 100px;">2</h1>
             </div>
             <div class="d-flex justify-content-center">
                 <a href="#" class="btn stretched-link">Open Incidents</a>
