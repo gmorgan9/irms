d4860acdd6e76ed71536b01d2e93ef125e9768f0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 17:46:17 -0600

    update

--- open-incidents.php
+++ open-incidents.php
@@ -51,7 +51,7 @@ session_start();
                 <h1 class="text-center" style="font-size: 100px;">
 
                 <?php
-                    echo "$rowtotal[0]";
+                    echo "$open_incidents($rowtotal[0])";
 
                 ?>
 
