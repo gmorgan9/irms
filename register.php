f69b38d1eca308a0f5bf83dbfc7a1bf08193c6c8
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 13:10:32 -0600

    update

--- register.php
+++ register.php
@@ -3,30 +3,28 @@ session_start();
     include("database/connection.php");
     include("database/functions.php");
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-        //something was posted
-        //$fullName = $_POST['fullName'];
-        //$emailAddress = $_POST['emailAddress'];
-        $userName = $_POST['userName'];
-        $password = $_POST['password'];
-        $confirmPassword = $_POST['confirmPassword'];
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
 
-        if(!empty($userName) && !empty($password) && !is_numeric($userName))
-        {
-            //save to database
-            $user_id = random_num(20);
-            $query = "insert into users (user_id,userName,password) values ('$user_id','$fullName','$userName','$password')";
-            
-            mysqli_query($query);
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
 
-            header("Location: login.php");
-            die;
-        } else 
-        {
-            echo "Please enter a valid username!";
-        }
-    }
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
 ?>
 
 <!DOCTYPE html>
