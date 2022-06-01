8ac6c652ce0463a1da66bbda51a436cdaa515761
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 16:16:31 -0600

    update

--- open-incidents.php
+++ open-incidents.php
@@ -55,18 +55,9 @@ session_start();
 <br><br><br>
 
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "Morgan22!", "irms");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
- 
 // Attempt select query execution
 $sql = "SELECT * FROM incidents";
-if($result = mysqli_query($link, $sql)){
+if($result = mysqli_query($con, $sql)){
     if(mysqli_num_rows($result) > 0){
         echo "<table>";
             echo "<tr>";
@@ -98,11 +89,11 @@ if($result = mysqli_query($link, $sql)){
         echo "No records matching your query were found.";
     }
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
 }
  
 // Close connection
-mysqli_close($link);
+mysqli_close($con);
 ?>
 
 
