5a7f158257c54b3e097457d5113209e7cf71d1e9
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 00:15:45 -0600

    m

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="assets/fontawesome/css/all.css" rel="stylesheet">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="assets/css/style.css?v=1.2">
+    <link rel="stylesheet" href="assets/css/style.css?v=1.3">
 
     <title>Home</title>
 </head>
