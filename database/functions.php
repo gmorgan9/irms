b039651d77a97a29912e88439bff795bb4e890dc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 14:24:23 -0600

    update

--- database/functions.php
+++ database/functions.php
@@ -73,7 +73,7 @@ if (isset($_POST['login_user'])) {
         $results = mysqli_query($con, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
-          $_SESSION['success'] = "You are now logged in";
+          //$_SESSION['success'] = "You are now logged in";
           header('location: /');
         }else {
             array_push($errors, "Wrong username/password combination");
