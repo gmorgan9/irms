0d6ae323e267723068e6e2bef3c81ed335ea4eb7
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 14:20:40 -0600

    update

--- database/functions.php
+++ database/functions.php
@@ -51,6 +51,32 @@ if (isset($_POST['submit'])) {
   	mysqli_query($con, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+  	header('location: /');
   }
 }
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+  
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($con, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['username'] = $username;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: /');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+  }
