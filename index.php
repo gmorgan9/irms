0c8cdac2e8af1ed3a05582486324c10961858257
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 14:21:05 -0600

    update

--- index.php
+++ index.php
@@ -3,7 +3,15 @@ session_start();
     include("database/connection.php");
     include("database/functions.php");
 
-   
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+    if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header("location: login.php");
+    }
 ?>
 
 <!DOCTYPE html>
