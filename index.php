b32093e2102e0f3a02fd6f39f35462140929dd1f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-31 23:39:39 -0600

    update

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-testing passed, IRMS is in.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <h2>
+            Incident Record Management System
+        </h2>
+        <div class="logout">
+        <a href="/logout.php"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+        </div>
+    </header>
+
+
+
+
+</body>
+</html>
