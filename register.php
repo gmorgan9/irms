198142fd98f76447b52f3a0a369c47015c4860bf
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 13:47:10 -0600

    update

--- register.php
+++ register.php
@@ -4,30 +4,97 @@ session_start();
     include("database/functions.php");
     
 
-    if(isset($_POST['registerBtn']))
-    {
-        //something was posted
-        $full_name = $_POST['full_name'];
-        $user_name = $_POST['user_name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        //$confirm_password = $_POST['confirm_password'];
-
-        if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-        {
-            //save to database
-            $user_id = random_num(20);
-            $query = "insert into users (user_id,full_name,user_name,email,password) values ('$user_id','$full_name','$user_name','$email','$password')";
+    // Define variables and initialize with empty values
+$user_name = $password = $confirm_password = "";
+$user_name_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["user_name"]))){
+        $user_name_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["user_name"]))){
+        $user_name_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE user_name = ?";
+        
+        if($stmt = mysqli_prepare($con, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_user_name);
             
-            mysqli_query($query);
+            // Set parameters
+            $param_user_name = trim($_POST["user_name"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $user_name_err = "This username is already taken.";
+                } else{
+                    $user_name = trim($_POST["user_name"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
-            header("Location: login.php");
-            die;
-        } else 
-        {
-            echo "Please enter a valid username!";
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
     }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($user_name_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (user_name, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_user_name, $param_password);
+            
+            // Set parameters
+            $param_user_name = $user_name;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($con);
+}
 ?>
 
 <!DOCTYPE html>
@@ -60,17 +127,6 @@ session_start();
 <div class="d-flex justify-content-center">
     <!-- form start -->
 <form action="register.php" class="reg-form" method="post">
-
-<?php
-		// check to see if the user successfully created an account
-		if (isset($success) && $success == true){
-			echo '<p color="green">Yay!! Your account has been created. <a href="./login.php">Click here</a> to login!<p>';
-		}
-		// check to see if the error message is set, if so display it
-		else if (isset($error_msg))
-			echo '<p color="red">'.$error_msg.'</p>';
-		
-	?>
 
 
 <div class="form-header d-flex justify-content-center">
