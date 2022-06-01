84818212570ba5892938c050841e0e9e22c4e837
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 10:55:54 -0600

    update

--- open-incidents.php
+++ open-incidents.php
@@ -9,7 +9,7 @@
     <link href="assets/fontawesome/css/all.css" rel="stylesheet">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="assets/css/style.css?v=1.99">
+    <link rel="stylesheet" href="assets/css/style.css?v=2.0">
 
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -33,9 +33,9 @@
 <div class="col d-flex justify-content-center">
 <div class="row">
 <div class="card open-count" style="width: 18rem;">
-        <div class="card-body">
-                <h1 class="text-center" style="font-size: 100px;">2</h1>
-                <p class="text-center">Open Incidents</p>
+    <div class="card-body">
+        <h1 class="text-center" style="font-size: 100px;">2</h1>
+        <p class="text-center">Open Incidents</p>
     </div>
 </div>
 <!-- <div class="card" style="width: 18rem;">
