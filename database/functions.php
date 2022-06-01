d421209c1d2925fa8c518482c3441e1ab3d6a74e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 14:17:16 -0600

    update

--- database/functions.php
+++ database/functions.php
@@ -7,22 +7,22 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', 'Morgan22!', 'irms');
+require_once('connection.php')
 
 // REGISTER USER
 if (isset($_POST['submit'])) {
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $password = mysqli_real_escape_string($db, $_POST['password_1']);
+  $confirm_password = mysqli_real_escape_string($db, $_POST['password_2']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
+  if (empty($password)) { array_push($errors, "Password is required"); }
+  if ($password != $confirm_password) {
 	array_push($errors, "The two passwords do not match");
   }
 
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = md5($password);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
