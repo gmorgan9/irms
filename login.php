325518fcd431a6fa5539086893facc0c964edbdf
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 10:26:19 -0600

    update

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
     <link href="assets/fontawesome/css/all.css" rel="stylesheet">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="assets/css/style.css?v=1.67">
+    <link rel="stylesheet" href="assets/css/style.css?v=1.94">
 
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -23,20 +23,44 @@
         </h2>
     </div>
 
-<br><br><br><br>
 
-<a href="/">back</a>
-
-<form class="form-inline">
-  <div class="form-group">
-    <label for="userName">Username</label>
-    <input type="text" id="userName" class="form-control mx-sm-3">
-
-    <label for="inputPassword6">Password</label>
-    <input type="password" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-  </div>
+<br><br><br>
+<div class="d-flex justify-content-center">
+<form>
+<div class="form-header d-flex justify-content-center">
+    <div class="bg-circle">
+        <div class="sm-circle">
+        <div class="d-flex justify-content-center">
+        <i class="user-header fa-solid fa-user fa-3x"></i>
+</div>
+        </div>
+    </div>
+</div>
+    <br><br>
+    
+    <div class="d-flex justify-content-center">
+    <div class="form-group input-group w-75">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-at"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="User Name" type="text">
+</div>
+    </div> <!-- form-group// -->
+    <div class="d-flex justify-content-center">
+    <div class="form-group input-group w-75">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input class="form-control" placeholder="Create password" type="password">
+</div>
+    </div> <!-- form-group// -->   
+    <div class="d-flex justify-content-center">                                
+    <button type="submit" class="btn btn-primary text-center reg-log">Create Account</button>  
+</div> 
+    <p class="text-center">Have an account? <a href="" style="color: black;">Log In</a> </p>                                                                 
 </form>
-
+</div>
+<a href="/">back</a>
 
 </body>
 </html>
