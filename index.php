5cb2aa045aa0ebba1980e8a671945f3800bd149b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 00:11:05 -0600

    m

--- index.php
+++ index.php
@@ -23,6 +23,15 @@
         </div>
     </header>
 
+    <div class="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+
 
 
 
