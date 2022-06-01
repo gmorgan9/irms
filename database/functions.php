8951bc9036c00c1bb2866bb3f314257a18818e28
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 15:45:08 -0600

    update

--- database/functions.php
+++ database/functions.php
@@ -95,47 +95,44 @@ $date    = "";
 $time    = "";
 $errors = array(); 
 
-  // RECORDING AN INCIDENT
+// RECORD AN INCIDENT
 if (isset($_POST['rec_inc'])) {
     // receive all input values from the form
     $inc_num = mysqli_real_escape_string($con, $_POST['inc_num']);
-    $priority = mysqli_real_escape_string($con, $_POST['priority']);
-    $description = mysqli_real_escape_string($con, $_POST['description']);
-    $assign_group = mysqli_real_escape_string($con, $_POST['assign_group']);
-    $kb_article = mysqli_real_escape_string($con, $_POST['kb_article']);
-    $date = mysqli_real_escape_string($con, $_POST['date']);
-    $time = mysqli_real_escape_string($con, $_POST['time']);
+    // $username = mysqli_real_escape_string($con, $_POST['username']);
+    // $email = mysqli_real_escape_string($con, $_POST['email']);
+    // $password = mysqli_real_escape_string($con, $_POST['password']);
+    // $confirm_password = mysqli_real_escape_string($con, $_POST['confirm_password']);
   
     // form validation: ensure that the form is correctly filled ...
     // by adding (array_push()) corresponding error unto $errors array
     if (empty($inc_num)) { array_push($errors, "Incident Number is required"); }
-    if (empty($priority)) { array_push($errors, "Priority is required"); }
-    if (empty($description)) { array_push($errors, "Description is required"); }
-    if (empty($assign_group)) { array_push($errors, "Assignment Group is required"); }
-    if (empty($kb_article)) { array_push($errors, "KB Article is required"); }
-    if (empty($date)) { array_push($errors, "Date is required"); }
-    if (empty($time)) { array_push($errors, "Time is required"); }
+    // if (empty($username)) { array_push($errors, "Username is required"); }
+    // if (empty($email)) { array_push($errors, "Email is required"); }
+    // if (empty($password)) { array_push($errors, "Password is required"); }
+    
   
     // first check the database to make sure 
     // a user does not already exist with the same username and/or email
     $user_check_query = "SELECT * FROM incidents WHERE inc_num='$inc_num' LIMIT 1";
     $result = mysqli_query($con, $user_check_query);
-    $inc_num = mysqli_fetch_assoc($result);
+    $inc = mysqli_fetch_assoc($result);
     
-    if ($inc_num) { // if user exists
-      if ($inc_num['inc_num'] === $inc_num) {
+    if ($inc) { // if user exists
+      if ($inc['inc_num'] === $inc_num) {
         array_push($errors, "Incident Number already exists");
       }
     }
   
     // Finally, register user if there are no errors in the form
     if (count($errors) == 0) {
+       // $password = md5($password);//encrypt the password before saving in the database
   
-        $query = "INSERT INTO incidents (inc_num, priority, description, assign_group, kb_article, date, time) 
-                  VALUES('$inc_num', '$priority', '$description', '$assign_group', '$kb_article', '$date', '$time')";
+        $query = "INSERT INTO incidents (inc_num) 
+                  VALUES('$inc_num')";
         mysqli_query($con, $query);
-        $_SESSION['inc_num'] = $inc_num;
-        $_SESSION['success'] = "Incdent Created!";
+        // $_SESSION['username'] = $username;
+        // $_SESSION['success'] = "You are now logged in";
         header('location: /');
     }
   }
