4d12eb908fe0aa74bb19378ff1298a4ff0e47b6d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-01 01:17:21 -0600

    update

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 <div class="row">
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <i class="fa-solid fa-envelope-open one"></i> <br>
+  <i class="fa-solid fa-envelope-open icon"></i> <br>
     <a href="#" class="btn stretched-link">Open Incident</a>
   </div>
 </div>
