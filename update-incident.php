4e2e3a3b3df8f7061e94c0d05f4e5ac8acf4a6ed
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 14:49:16 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -3,16 +3,16 @@ session_start();
     include("database/connection.php");
     // include("database/functions.php");
     $id=$_GET['updateid'];
-    // $sql = "SELECT * FROM incidents where id=$id";
-    // $result=mysqli_query($con,$sql);
-    // $row=mysqli_fetch_assoc($result);
-    // $inc_num = $row['inc_num'];
-    // $priority = $row['priority'];
-    // $description = $row['description'];
-    // $assign_group = $row['assign_group'];
-    // $kb_article = $row['kb_article'];
-    // $date = $row['date'];
-    // $time = $row['time'];
+    $sql = "SELECT * FROM incidents where id=$id";
+    $result=mysqli_query($con,$sql);
+    $row=mysqli_fetch_assoc($result);
+    $inc_num = $row['inc_num'];
+    $priority = $row['priority'];
+    $description = $row['description'];
+    $assign_group = $row['assign_group'];
+    $kb_article = $row['kb_article'];
+    $date = $row['date'];
+    $time = $row['time'];
 
 
     if(isset($_POST['submit'])) {
