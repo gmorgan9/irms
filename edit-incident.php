449e723408d4d68014932f2bd55b803f331ff51f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 11:59:16 -0600

    update

--- edit-incident.php
+++ edit-incident.php
@@ -4,9 +4,22 @@ session_start();
     include("database/functions.php");
 
     $results = mysqli_query($con, "SELECT * FROM incidents where inc_id='$inc_id'");
-    if (isset($_GET['edit-incident'])) {
-		$incident = $results;
-	}
+    // if (isset($_GET['edit-incident'])) {
+	// 	$incident = $results;
+	// }
+
+    if(isset($_POST['update_incident']))
+{
+    $inc_num = $_POST['inc_num'];
+    $priority = $_POST['priority'];
+    $description = $_POST['description'];
+    $assign_group = $_POST['assign_group'];
+    $kb_article = $_POST['kb_article'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];	
+$result = mysqli_query($mysqli, "UPDATE incidents SET inc_num='$inc_num', priority='$priority', description='$description', assign_group='$assign_group', kb_article='$kb_article', date='$date', time='$time' WHERE inc_id=$inc_id");
+header("Location: /"); 
+}
 
 
     //$all_incidents = getAllInc();
