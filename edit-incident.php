eaa6f4f446b87c27e0cf7aa3085ddf3ad02dea64
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 09:56:56 -0600

    update

--- edit-incident.php
+++ edit-incident.php
@@ -5,7 +5,7 @@ session_start();
 
 
     if (isset($_GET['inc_id'])) {
-		$update_incident = updateIncident($_GET['inc_id']);
+		$upd_inc = updateIncident($_GET['inc_id']);
 	}
 
 
@@ -62,7 +62,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa-solid fa-arrow-up-wide-short"></i> </span>
 		    </div>
-            <input name="priority" class="form-control" placeholder="Priority" type="text" value="<?php echo $update_incident['priority']; ?>">
+            <input name="priority" class="form-control" placeholder="Priority" type="text" value="<?php echo $upd_inc['priority']; ?>">
         </div>
     </div> <!-- form-group// -->
     <div class="d-flex justify-content-center">
