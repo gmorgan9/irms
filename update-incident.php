4d313dc5e6d650e1c5ae9640f532c7d8e294e35a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 16:22:20 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -106,21 +106,19 @@
 <br>
 
 <div class="d-flex justify-content-center">
-<?php
-                        if(isset($_GET['updateid']))
-                        {
-                            $id=$_GET['updateid'];
-                            $sql = "SELECT * FROM incidents where id=$id";
-                            $result=mysqli_query($con,$sql);
-                            $row=mysqli_fetch_assoc($result);
-                            $inc_num = $row['inc_num'];
-                            $priority = $row['priority'];
-                            $description = $row['description'];
-                            $assign_group = $row['assign_group'];
-                            $kb_article = $row['kb_article'];
-                            $date = $row['date'];
-                            $time = $row['time'];
-                                ?>
+<?php     
+    $id=$_GET['updateid'];
+    $sql = "SELECT * FROM incidents where id=$id";
+    $result=mysqli_query($con,$sql);
+    $row=mysqli_fetch_assoc($result);
+    $inc_num = $row['inc_num'];
+    $priority = $row['priority'];
+    $description = $row['description'];
+    $assign_group = $row['assign_group'];
+    $kb_article = $row['kb_article'];
+    $date = $row['date'];
+    $time = $row['time'];
+?>
     <!-- form start -->
 <form action="update-incident.php" class="reg-form" method="post">
 <?php //include('errors.php'); ?>
