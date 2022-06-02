6f689c5516efc29aaf92b7e96ca585993132e2d5
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 08:55:01 -0600

    update

--- edit-incident.php
+++ edit-incident.php
@@ -47,7 +47,7 @@ session_start();
             <div class="input-group-prepend">
 	            <span class="input-group-text"> <i class="fa-solid fa-hashtag"></i> </span>
 	        </div>
-            <input name="inc_num" class="form-control" placeholder="Incident Number" type="text" value="<?php echo $inc_num ?>">
+            <input name="inc_num" class="form-control" placeholder="Incident Number" type="text" value="<?php echo $inc_num; ?>">
         </div>
     </div> 
     <!-- form-group// -->
@@ -56,7 +56,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa-solid fa-arrow-up-wide-short"></i> </span>
 		    </div>
-            <input name="priority" class="form-control" placeholder="Priority" type="text" value="<?php echo $priority ?>">
+            <input name="priority" class="form-control" placeholder="Priority" type="text" value="<?php echo $priority; ?>">
         </div>
     </div> <!-- form-group// -->
     <div class="d-flex justify-content-center">
@@ -64,7 +64,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa-solid fa-pen-to-square"></i> </span>
 		    </div>
-            <input name="description" class="form-control" placeholder="Description" type="text" value="<?php echo $description ?>">
+            <input name="description" class="form-control" placeholder="Description" type="text" value="<?php echo $description; ?>">
         </div>
     </div> <!-- form-group// -->
     <div class="d-flex justify-content-center">
@@ -72,7 +72,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa fa-users fa-xs"></i> </span>
 		    </div>
-            <input name="assign_group" class="form-control" placeholder="Assignment Group" type="text" value="<?php echo $assign_group ?>">
+            <input name="assign_group" class="form-control" placeholder="Assignment Group" type="text" value="<?php echo $assign_group; ?>">
         </div>
     </div> <!-- form-group// -->
     <div class="d-flex justify-content-center">
@@ -80,7 +80,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa fa-book"></i> </span>
 		    </div>
-            <input name="kb_article" class="form-control" placeholder="KB Artcile" type="text" value="<?php echo $kb_article ?>">
+            <input name="kb_article" class="form-control" placeholder="KB Artcile" type="text" value="<?php echo $kb_article; ?>">
         </div>
     </div> <!-- form-group// -->    
     <div class="d-flex justify-content-center">
@@ -88,7 +88,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa fa-calendar-days"></i> </span>
 		    </div>
-            <input name="date" class="form-control" placeholder="Date" type="date" value="<?php echo $date ?>">
+            <input name="date" class="form-control" placeholder="Date" type="date" value="<?php echo $date; ?>">
         </div>
     </div> <!-- form-group// -->    
     <div class="d-flex justify-content-center">
@@ -96,7 +96,7 @@ session_start();
     	    <div class="input-group-prepend">
 		        <span class="input-group-text"> <i class="fa fa-clock"></i> </span>
 		    </div>
-            <input name="time" class="form-control" placeholder="Time" type="time" value="<?php echo $time ?>">
+            <input name="time" class="form-control" placeholder="Time" type="time" value="<?php echo $time; ?>">
         </div>
     </div> <!-- form-group// -->   
     <div class="d-flex justify-content-center">                                
