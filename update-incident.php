69158f092b0d0c8f0a45715e4615d44b88571d42
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 13:50:21 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -13,7 +13,7 @@ session_start();
         $date = $row['date'];
         $time = $row['time'];
 
-        $sql = "UPDATE incidents SET inc_id=$inc_id, inc_num='$inc_num', priority='$priority', description='$description', assign_group='$assign_group', kb_article='$kb_article', date='$date', time='$time' WHERE inc_id='$inc_id'";
+        $sql = "UPDATE incidents SET inc_id='$inc_id', inc_num='$inc_num', priority='$priority', description='$description', assign_group='$assign_group', kb_article='$kb_article', date='$date', time='$time' WHERE inc_id='$inc_id'";
         $result=mysqli_query($con,$sql);
         if($result) {
             echo "Updated Successfully";
