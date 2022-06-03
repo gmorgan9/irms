7a1ab4a54e4b493d4d6a6639598cd07001f3a258
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 18:48:44 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -2,7 +2,7 @@
 session_start();
     include("database/connection.php");
 
-    $id=(int)$_GET['id'];
+    $id=$_GET['updateid'];
     $sql = "SELECT * FROM incidents where id='$id' limit 1";
     $result=mysqli_query($con,$sql);
     $row=mysqli_fetch_assoc($result);
