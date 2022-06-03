b8ec92be258f2ae6c125ab7a924253c4d07ee4a5
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 17:17:20 -0600

    udpate

--- incident-notes.php
+++ incident-notes.php
@@ -112,7 +112,7 @@ session_start();
             <th scope="row"><?php echo $id; ?></th>
             <td scope="row"><?php echo $date; ?></td>
             <td scope="row"><?php echo $title; ?></td>
-            <td scope="row" class="col-2 text-truncate text-nowrap" style="max-width: 30em;"><?php echo $note; ?></td>
+            <td style="width: 250px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"><?php echo $note; ?></td>
             <td scope="row"><?php echo $tag; ?></td>
             <td scope="row"><?php echo $time; ?></td>
             <td scope="row"><a href="update-note.php?updateid=<?php echo $id; ?>"><i class="fa-solid fa-pen-to-square"></a></i></td>
