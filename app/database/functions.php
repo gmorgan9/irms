c7abc78234589a7b40f804aed61fa5b72bd2b403
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 19:56:56 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -1,129 +1,129 @@
 <?php 
-session_start();
+// session_start();
 
-// connect to database update
-$db = mysqli_connect('localhost', 'gmorg', 'gmorgpass', 'finance');
+// // connect to database update
+// $db = mysqli_connect('localhost', 'gmorg', 'gmorgpass', 'finance');
 
-// variable declaration
-$username = "";
-$email    = "";
-$errors   = array(); 
+// // variable declaration
+// $username = "";
+// $email    = "";
+// $errors   = array(); 
 
-	function executeQuery($sql, $data)
-{
-    global $db;
-    $stmt = $db->prepare($sql);
-    $values = array_values($data);
-    $types = str_repeat('s', count($values));
-    $stmt->bind_param($types, ...$values);
-    $stmt->execute();
-    return $stmt;
-}
+// 	function executeQuery($sql, $data)
+// {
+//     global $db;
+//     $stmt = $db->prepare($sql);
+//     $values = array_values($data);
+//     $types = str_repeat('s', count($values));
+//     $stmt->bind_param($types, ...$values);
+//     $stmt->execute();
+//     return $stmt;
+// }
 
 
-function dd($value) // to be deleted
-{
-    echo "<pre>", print_r($value, true), "</pre>";
-    die();
-}
+// function dd($value) // to be deleted
+// {
+//     echo "<pre>", print_r($value, true), "</pre>";
+//     die();
+// }
 
-// SELECT ALL
-	function selectAll($table, $conditions = [])
-	{
-		global $db;
-		$sql = "SELECT * FROM $table";
-		if (empty($conditions)) {
-			$stmt = $db->prepare($sql);
-			$stmt->execute();
-			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-			return $records;
-		} else {
-			$i = 0;
-			foreach ($conditions as $key => $value) {
-				if ($i === 0) {
-					$sql = $sql . " WHERE $key=?";
-				} else {
-					$sql = $sql . " AND $key=?";
-				}
-				$i++;
-			}
+// // SELECT ALL
+// 	function selectAll($table, $conditions = [])
+// 	{
+// 		global $db;
+// 		$sql = "SELECT * FROM $table";
+// 		if (empty($conditions)) {
+// 			$stmt = $db->prepare($sql);
+// 			$stmt->execute();
+// 			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+// 			return $records;
+// 		} else {
+// 			$i = 0;
+// 			foreach ($conditions as $key => $value) {
+// 				if ($i === 0) {
+// 					$sql = $sql . " WHERE $key=?";
+// 				} else {
+// 					$sql = $sql . " AND $key=?";
+// 				}
+// 				$i++;
+// 			}
 			
-			$stmt = executeQuery($sql, $conditions);
-			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-			return $records;
-		}
-	}
-// SELECT ONE
-function selectOne($table, $conditions)
-{
-    global $db;
-    $sql = "SELECT * FROM $table";
+// 			$stmt = executeQuery($sql, $conditions);
+// 			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+// 			return $records;
+// 		}
+// 	}
+// // SELECT ONE
+// function selectOne($table, $conditions)
+// {
+//     global $db;
+//     $sql = "SELECT * FROM $table";
 
-    $i = 0;
-    foreach ($conditions as $key => $value) {
-        if ($i === 0) {
-            $sql = $sql . " WHERE $key=?";
-        } else {
-            $sql = $sql . " AND $key=?";
-        }
-        $i++;
-    }
+//     $i = 0;
+//     foreach ($conditions as $key => $value) {
+//         if ($i === 0) {
+//             $sql = $sql . " WHERE $key=?";
+//         } else {
+//             $sql = $sql . " AND $key=?";
+//         }
+//         $i++;
+//     }
 
-    $sql = $sql . " LIMIT 1";
-    $stmt = executeQuery($sql, $conditions);
-    $records = $stmt->get_result()->fetch_assoc();
-    return $records;
-}
-// CREATE
-function create($table, $data)
-{
-    global $db;
-    $sql = "INSERT INTO $table SET ";
+//     $sql = $sql . " LIMIT 1";
+//     $stmt = executeQuery($sql, $conditions);
+//     $records = $stmt->get_result()->fetch_assoc();
+//     return $records;
+// }
+// // CREATE
+// function create($table, $data)
+// {
+//     global $db;
+//     $sql = "INSERT INTO $table SET ";
 
-    $i = 0;
-    foreach ($data as $key => $value) {
-        if ($i === 0) {
-            $sql = $sql . " $key=?";
-        } else {
-            $sql = $sql . ", $key=?";
-        }
-        $i++;
-    }
+//     $i = 0;
+//     foreach ($data as $key => $value) {
+//         if ($i === 0) {
+//             $sql = $sql . " $key=?";
+//         } else {
+//             $sql = $sql . ", $key=?";
+//         }
+//         $i++;
+//     }
     
-    $stmt = executeQuery($sql, $data);
-    $id = $stmt->insert_id;
-    return $id;
-}
+//     $stmt = executeQuery($sql, $data);
+//     $id = $stmt->insert_id;
+//     return $id;
+// }
 
-function update($table, $id, $data)
-{
-    global $db;
-    $sql = "UPDATE $table SET ";
+// function update($table, $id, $data)
+// {
+//     global $db;
+//     $sql = "UPDATE $table SET ";
 
-    $i = 0;
-    foreach ($data as $key => $value) {
-        if ($i === 0) {
-            $sql = $sql . " $key=?";
-        } else {
-            $sql = $sql . ", $key=?";
-        }
-        $i++;
-    }
+//     $i = 0;
+//     foreach ($data as $key => $value) {
+//         if ($i === 0) {
+//             $sql = $sql . " $key=?";
+//         } else {
+//             $sql = $sql . ", $key=?";
+//         }
+//         $i++;
+//     }
 
-    $sql = $sql . " WHERE id=?";
-    $data['id'] = $id;
-    $stmt = executeQuery($sql, $data);
-    return $stmt->affected_rows;
-}
+//     $sql = $sql . " WHERE id=?";
+//     $data['id'] = $id;
+//     $stmt = executeQuery($sql, $data);
+//     return $stmt->affected_rows;
+// }
 
-function delete($table, $id)
-{
-    global $db;
-    $sql = "DELETE FROM $table WHERE id=?";
+// function delete($table, $id)
+// {
+//     global $db;
+//     $sql = "DELETE FROM $table WHERE id=?";
 
-    $stmt = executeQuery($sql, ['id' => $id]);
-    return $stmt->affected_rows;
-}
+//     $stmt = executeQuery($sql, ['id' => $id]);
+//     return $stmt->affected_rows;
+// } 
 
 // function getPublishedPosts()
 // {
