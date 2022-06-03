4a796de6ef4b48def39eb5358c614a78dfc7e47a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 10:57:53 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -245,7 +245,7 @@ if(isset($_POST["update"])){
             <div class="input-group-prepend">
 	            <span class="input-group-text"><i class="fa-solid fa-hashtag"></i></span>
 	        </div>
-            <input name="status" class="form-control text-center" placeholder="Status" type="text" value="<?php echo $status ?>" readonly>
+            <input name="status" class="form-control text-center" placeholder="Status" type="checkbox">
         </div>
     </div> 
     <!-- form-group// -->
