02efc079a547616d30d55b13bd04d64430c522f5
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 21:17:10 -0600

    update

--- admin/topics/create.php
+++ admin/topics/create.php
@@ -11,19 +11,23 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
         <!-- Font Awesome -->
-  <link href="assets/fontawesome/css/all.css" rel="stylesheet">
+        <link href="assets/fontawesome/css/all.css" rel="stylesheet">
 
-<!-- Favicon -->
-<link rel="icon" type="image/x-icon" href="/assets/images/fav.png?v=<?php echo time(); ?>">
+            <!-- Bootstrap Styles -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+            rel="stylesheet">
+        
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="/assets/images/fav.png?v=<?php echo time(); ?>">
 
-<!-- Custom Styling -->
-<link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+        <!-- Custom Styling -->
+        <link rel="stylesheet" href="../../assets/css/style.css">
 
-<!-- Bootstrap Styles -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Admin Styling -->
+        <link rel="stylesheet" href="../../assets/css/admin.css">
 
         <title>Admin Section - Add Topic</title>
     </head>
