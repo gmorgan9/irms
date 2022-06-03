a3761ab86ea8455317887ddd975e05b92bb24809
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 16:58:47 -0600

    m

--- incident-notes.php
+++ incident-notes.php
@@ -90,7 +90,7 @@ session_start();
       <th scope="col">#</th>
       <th scope="col">Date</th>
       <th scope="col">Title</th>
-      <th class="w-50" scope="col">Note</th>
+      <th class="w-75" scope="col">Note</th>
       <th scope="col">Tag</th>
     </tr>
   </thead>
