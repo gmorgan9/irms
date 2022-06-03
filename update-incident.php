8a5cc4096b230db91394ed28cde03191ca63d461
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 18:22:32 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -26,6 +26,8 @@ session_start();
 
         // Update Statement
         $sql = "UPDATE `incidents` SET `inc_num`='$inc_num',`priority`='$priority',`description`='$description',`assign_group`='$assign_group',`kb_article`='$kb_article',`date`='$date',`time`='$time' HAVING id=$id";
+
+        mysqli_query($con,"UPDATE incidents set inc_num='" . $_POST['inc_num'] . "', priority='" . $_POST['priority'] . "', description='" . $_POST['description'] . "', assign_group='" . $_POST['assign_group'] . "' ,kb_article='" . $_POST['kb_article'] . "' ,date='" . $_POST['date'] . "' ,time='" . $_POST['time'] . "' where id='" . $_POST['id'] . "'");
         $result=mysqli_query($con,$sql);
         if($result) {
             echo "Updated Successfully";
