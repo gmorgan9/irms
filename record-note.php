24c970674cec0b9f56869b0d31bec371db06827e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 16:27:22 -0600

    update

--- record-note.php
+++ record-note.php
@@ -21,8 +21,8 @@ session_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- CKEDITOR -->
-    <script src="//cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
-    <script src="ckeditor/ckeditor.js"></script>
+    <!-- <script src="//cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
+    <script src="ckeditor/ckeditor.js"></script> -->
 
     <title>Record Note - IRMS</title>
 </head>
