3f4ce73d7082e067b253970ecef4a8c767012fbc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 23:30:51 -0600

    update

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ session_start();
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
 		</div>
-        <input name="password" class="form-control" placeholder="Enter password" type="password" id="password">
+        <input name="password" class="form-control" placeholder="Enter password" type="password" id="password"><br>
         <input type="checkbox" id="checkbox">Show Password
 </div>
     </div> <!-- form-group// -->   
