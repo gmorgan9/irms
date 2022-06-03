1c861be33b972bfc058907d163b11c883bc6d07e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 13:04:24 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -95,7 +95,7 @@ if(isset($_POST["update"])){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records updated successfully. Redirect to landing page
-                header("location:javascript://history.go(-2)");
+                header("location: index.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
