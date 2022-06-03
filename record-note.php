2695eee6f009f1baedde8294ce531f358dfa6c28
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 16:08:38 -0600

    update

--- record-note.php
+++ record-note.php
@@ -83,7 +83,7 @@ session_start();
 
     <div class="form-row">
         <div class="d-flex justify-content-center">
-            <div class="form-group input-group w-50">
+            <div class="form-group input-group">
     	        <div class="input-group-prepend">
 		            <span class="input-group-text"> <i class="fa fa-users fa-xs"></i> </span>
 		        </div>
