6e9d2648ff4c4f2b19437fe360945d56acf8bff3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 12:59:37 -0600

    update

--- open-incidents.php
+++ open-incidents.php
@@ -7,8 +7,23 @@ session_start();
         $_SESSION['msg'] = "You must log in first";
         header('location: login.php');
     }
-    $open_incidents = getOpenInc();
-    $count_open_incidents = countOpenInc();
+
+    // DELETE
+    if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+    
+        $sql = "DELETE FROM incidents WHERE id=$id";
+        $result = mysqli_query($con, $sql);
+        if($result) {
+            // echo "Deleted Successfully";
+            header('location: all-incidents.php'); // returns back to same page
+        } else {
+            die(mysqli_error($con));
+        }
+    }
+
+    // UPDATE
+
 ?>
 
 
@@ -23,7 +38,7 @@ session_start();
     <link href="assets/fontawesome/css/all.css" rel="stylesheet">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="assets/css/style.css?v=2.14">
+    <link rel="stylesheet" href="assets/css/style.css?v=2.15">
 
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -59,31 +74,21 @@ session_start();
                 ?>
 
                 </h1>
-                <p class="text-center">Open Incidents</p>
+                <p class="text-center">Closed Incidents</p>
             </div>
         </div>
     </div>
 </div>
 <br><br><br>
 
-
-
-
-
-
-
-
-
 <br><br><br>
-
-
-
 
 <div class="col d-flex justify-content-center">
 <table class="table table-hover table-light">
   <thead>
     <tr class="header-line">
       <th scope="col">#</th>
+      <th scope="col">Status</th>
       <th scope="col">Incident Number</th>
       <th scope="col">Severity</th>
       <th scope="col">Description</th>
@@ -94,42 +99,44 @@ session_start();
     </tr>
   </thead>
   <tbody>
-  <?php
 
-$sql = "SELECT * FROM incidents";
-$result = mysqli_query($con, $sql);
-if($result) {
-    while ($row = mysqli_fetch_assoc($result)) {
-      $id=$row['id'];
-      $status=$row['status'];
-      $inc_num = $row['inc_num'];
-      $priority = $row['priority'];
-      $description = $row['description'];
-      $assign_group = $row['assign_group'];
-      $kb_article = $row['kb_article'];
-      $date = $row['date'];
-      $time = $row['time'];
-      ?>
-      <tr>
-      <th scope="row"><?php echo $id; ?></th>
-      <?php if($status == 0) { ?>
-          <td>open</td>
-      <?php } else { ?>
-      <td>closed</td>
-      <?php } ?>
-      <td><?php echo $inc_num; ?></td>
-      <td><?php echo $priority; ?></td>
-      <td style="max-width: 30em;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100px;"><?php echo $description ?></td>
-      <td><?php echo $assign_group; ?></td>
-      <td><?php echo $ekb_article; ?></td>
-      <td><?php echo $date; ?></td>
-      <td><?php echo $time; ?></td>
-      <td><a href="update-incident.php?updateid=<?php echo $id; ?>"><i class="fa-solid fa-pen-to-square"></a></i></td>
-      <td><a href="all-incidents.php?id=<?php echo $id; ?>" class="delete"><i class="fa-solid fa-trash-can"></i></a></td>
-      </tr>
-   <?php }
-}
-    
+      <?php
+
+      $sql = "SELECT * FROM incidents";
+      $result = mysqli_query($con, $sql);
+      if($result) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $id=$row['id'];
+            $status=$row['status'];
+            $inc_num = $row['inc_num'];
+            $priority = $row['priority'];
+            $description = $row['description'];
+            $assign_group = $row['assign_group'];
+            $kb_article = $row['kb_article'];
+            $date = $row['date'];
+            $time = $row['time'];
+            ?>
+            <tr>
+            <th scope="row"><?php echo $id; ?></th>
+            <?php if($status == 0) { ?>
+                <td>open</td>
+            <?php } else { ?>
+            <td>closed</td>
+            <?php } ?>
+            <td><?php echo $inc_num; ?></td>
+            <td><?php echo $priority; ?></td>
+            <td style="max-width: 30em;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100px;"><?php echo $description ?></td>
+            <td><?php echo $assign_group; ?></td>
+            <td><?php echo $ekb_article; ?></td>
+            <td><?php echo $date; ?></td>
+            <td><?php echo $time; ?></td>
+            <td><a href="update-incident.php?updateid=<?php echo $id; ?>"><i class="fa-solid fa-pen-to-square"></a></i></td>
+            <td><a href="all-incidents.php?id=<?php echo $id; ?>" class="delete"><i class="fa-solid fa-trash-can"></i></a></td>
+            </tr>
+         <?php }
+      }
+
+?>
   </tbody>
 </table>
 </div>
