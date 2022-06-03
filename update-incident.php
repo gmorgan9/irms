9da5966f5ff984867d1720756a5300a275e9f2af
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 12:36:00 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -237,7 +237,7 @@ if(isset($_POST["update"])){
             <div class="input-group-prepend">
 	            <span class="input-group-text">Status</span>
 	        </div>
-            <input name="id" class="form-control text-center" placeholder="Incident Number" type="text" value="<?php echo $status ?>">
+            <input name="status" class="form-control text-center" placeholder="Incident Number" type="checkbox" checked>
         </div>
     </div> 
     <!-- form-group// -->
@@ -247,7 +247,7 @@ if(isset($_POST["update"])){
             <div class="input-group-prepend">
 	            <span class="input-group-text">Status</span>
 	        </div>
-            <input name="id" class="form-control text-center" placeholder="Incident Number" type="text" value="<?php echo $status ?>">
+            <input name="status" class="form-control text-center" placeholder="Incident Number" type="text">
         </div>
     </div> 
     <!-- form-group// -->
