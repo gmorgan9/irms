21c792eb5f835e2f35e0a474b45bea4558d36215
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 14:03:14 -0600

    update

--- priority-report.php
+++ priority-report.php
@@ -95,7 +95,7 @@ session_start();
         <div class="card-body">
             <div class="d-flex justify-content-center">
             
-                <p class="text-center" style="font-size: 33.5px;">
+                <p class="text-center" style="font-size: 33.75px;">
                 <?php
             $sql="select count('1') from incidents where priority=4";
             $result=mysqli_query($con,$sql);
