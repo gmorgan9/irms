37c677f2bd458e89a2923d4cea8fce20de1ef523
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 10:32:23 -0600

    udpate

--- update-incident.php
+++ update-incident.php
@@ -5,7 +5,7 @@ session_start();
 
 
    // Define variables and initialize with empty values
-$inc_num = $priority = $description = $assign_group = $kb_article = $date = $time = "";
+$inc_num = $priority = $description = $assign_group = $kb_article = "";
 $inc_num_err = $priority_err = "";
  
 // Processing form data when form is submitted
@@ -33,11 +33,11 @@ if(isset($_POST["update"])){
     // Check input errors before inserting in database
     if(empty($inc_num_err) && empty($priority_err)){
         // Prepare an update statement
-        $sql = "UPDATE incidents SET inc_num=?, priority=?, description=?, assign_group=?, kb_article=?, date=?, time=? WHERE id=?";
+        $sql = "UPDATE incidents SET inc_num=?, priority=?, description=?, assign_group=?, kb_article=? WHERE id=?";
          
         if($stmt = mysqli_prepare($con, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssssssi", $param_inc_num, $param_priority, $param_description, $param_assign_group, $param_kb_article, $param_date, $param_time, $param_id);
+            mysqli_stmt_bind_param($stmt, "sssssssi", $param_inc_num, $param_priority, $param_description, $param_assign_group, $param_kb_article, $param_id);
             
             // Set parameters
             $param_inc_num = $inc_num;
@@ -45,8 +45,8 @@ if(isset($_POST["update"])){
             $param_description = $description;
             $param_assign_group = $assign_group;
             $param_kb_article = $kb_article;
-            $param_date = $date;
-            $param_time = $time;
+            //$param_date = $date;
+            //$param_time = $time;
             $param_id = $id;
             
             // Attempt to execute the prepared statement
