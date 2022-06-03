49a39fd1d610f253773aee049425743bf9ce0c03
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 20:30:00 -0600

    update

--- login.php
+++ login.php
@@ -31,6 +31,48 @@
 
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
+
+<div class="d-flex justify-content-center">
+<form action="login.php" class="log-form" method="post">
+<?php //include('errors.php'); ?>
+
+<div class="form-header d-flex justify-content-center">
+    <div class="bg-circle">
+        <div class="sm-circle">
+        <div class="d-flex justify-content-center">
+        <i class="user-header fa-solid fa-user fa-3x"></i>
+</div>
+        </div>
+    </div>
+</div>
+<br>
+<h2 class="text-center">Member Login</h2>
+    <br>
+    
+    <div class="d-flex justify-content-center">
+    <div class="form-group input-group w-75">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-at"></i> </span>
+		 </div>
+        <input name="username" class="form-control" placeholder="User Name" type="text">
+</div>
+    </div> <!-- form-group// -->
+    <div class="d-flex justify-content-center">
+    <div class="form-group input-group w-75">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input name="password" class="form-control" placeholder="Enter password" type="password">
+</div>
+    </div> <!-- form-group// -->   
+    <div class="d-flex justify-content-center">                                
+    <button type="submit" name="login-btn" class="btn btn-primary text-center reg-log">Log In</button>  
+</div> 
+    <p class="text-center">Don't have an account? <a href="/register.php" style="color: black;">Register</a> </p>                                                                 
+</form>
+</div>
+
+
   <div class="auth-content">
 
     <form action="login.php" method="post">
