1372f74bfabfd7288db8584412c62a393689713f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 20:51:38 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -51,26 +51,27 @@ function register(){
 	if (count($errors) == 0) {
 		$password = md5($password);//encrypt the password before saving in the database
 
-		// if (isset($_POST['user_type'])) {
-		// 	$user_type = e($_POST['user_type']);
-		// 	$query = "INSERT INTO users (name, username, email, user_type, password) 
-		// 			  VALUES('$name', '$username', '$email', '$user_type', '$password')";
-		// 	mysqli_query($db, $query);
-		// 	$_SESSION['success']  = "New user successfully created!!";
-		// 	header('location: /');
-		// }else{
+		if (!isset($_POST['user_type'])) {
 			$query = "INSERT INTO users (name, username, email, user_type, password) 
 					  VALUES('$name','$username', '$email', 'user', '$password')";
-			mysqli_query($db, $query);
-
-			// get id of the created user
-			$logged_in_user_id = mysqli_insert_id($db);
+            mysqli_query($db, $query);
+            $logged_in_user_id = mysqli_insert_id($db);
 
 			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
 			$_SESSION['success']  = "You are now logged in";
-			header('location: /');				
+			header('location: /');
+		}else{
+			
+                      $user_type = e($_POST['user_type']);
+                      $query = "INSERT INTO users (name, username, email, user_type, password) 
+                                VALUES('$name', '$username', '$email', '$user_type', '$password')";
+                      mysqli_query($db, $query);
+                      $_SESSION['success']  = "New user successfully created!!";
+                      header('location: /');
+							
 		}
 	}
+}
 
 // call the register() function if register_btn is clicked
 if (isset($_POST['create_user_btn'])) {
