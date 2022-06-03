2052433b7797f5c6575d521776fec26b07c73143
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 18:27:13 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -2,7 +2,7 @@
 session_start();
     include("database/connection.php");
 
-    $id=$_GET['id'];
+    $id=trim($_GET['id']);
     $sql = "SELECT * FROM `incidents` where id=$id limit 1";
     $result=mysqli_query($con,$sql);
     $row=mysqli_fetch_assoc($result);
@@ -16,27 +16,27 @@ session_start();
 
 
     if (isset($_POST['update'])) {
-        // $inc_num = $_POST['inc_num'];
-        // $priority = $_POST['priority'];
-        // $description = $_POST['description'];
-        // $assign_group = $_POST['assign_group'];
-        // $kb_article = $_POST['kb_article'];
-        // $date = $_POST['date'];
-        // $time = $_POST['time'];
+        $inc_num = $_POST['inc_num'];
+        $priority = $_POST['priority'];
+        $description = $_POST['description'];
+        $assign_group = $_POST['assign_group'];
+        $kb_article = $_POST['kb_article'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
 
         // Update Statement
-        // $sql = "UPDATE `incidents` SET `inc_num`='$inc_num',`priority`='$priority',`description`='$description',`assign_group`='$assign_group',`kb_article`='$kb_article',`date`='$date',`time`='$time' HAVING id=$id";
-        if(count($_POST)>0) {
-            mysqli_query($con,"UPDATE incidents set inc_num='" . $_POST['inc_num'] . "', priority='" . $_POST['priority'] . "', description='" . $_POST['description'] . "', assign_group='" . $_POST['assign_group'] . "' ,kb_article='" . $_POST['kb_article'] . "' ,date='" . $_POST['date'] . "' ,time='" . $_POST['time'] . "' WHERE id='" . $_POST['id'] . "'");
-        $message = "Record Modified Successfully";
-        }
-        //$result=mysqli_query($con,$sql);
-        //if($result) {
-        //    echo "Updated Successfully";
+        $sql = "UPDATE `incidents` SET `inc_num`='$inc_num',`priority`='$priority',`description`='$description',`assign_group`='$assign_group',`kb_article`='$kb_article',`date`='$date',`time`='$time' HAVING id=$id";
+        // if(count($_POST)>0) {
+        //     mysqli_query($con,"UPDATE incidents set inc_num='" . $_POST['inc_num'] . "', priority='" . $_POST['priority'] . "', description='" . $_POST['description'] . "', assign_group='" . $_POST['assign_group'] . "' ,kb_article='" . $_POST['kb_article'] . "' ,date='" . $_POST['date'] . "' ,time='" . $_POST['time'] . "' where id='" . $_POST['id'] . "'");
+        // $message = "Record Modified Successfully";
+        // }
+        $result=mysqli_query($con,$sql);
+        if($result) {
+            echo "Updated Successfully";
             // header('location: all-incidents.php');
-        // } else {
-        //    die(mysqli_error($con));
-        //}
+        } else {
+            die(mysqli_error($con));
+        }
     }
 
 ?>
