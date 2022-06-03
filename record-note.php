507193337ca61424e74f29fb05fc91627a67a1ed
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 15:51:02 -0600

    update

--- record-note.php
+++ record-note.php
@@ -20,6 +20,9 @@ session_start();
     <!-- Bootstrap Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <!-- CKEDITOR -->
+    <script src="//cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
+
     <title>Record Note - IRMS</title>
 </head>
 <body>
@@ -78,20 +81,12 @@ session_start();
 </form>
 </div>
 
+<script src='tinymce/tinymce.min.js'>
 
-<div id="editor">
-	<p>This is the editor content.</p>
-</div>
-<script src="./node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js"></script>
-<script>
-	ClassicEditor
-		.create( document.querySelector( '#editor' ) )
-		.then( editor => {
-			window.editor = editor;
-		} )
-		.catch( error => {
-			console.error( 'There was a problem initializing the editor.', error );
-		} );
+tinymce.init({
+    selector: '#myTextarea'
+});
+
 </script>
 
 </body>
