56ec4fcd005c4ff7064e353c401e27b4cc50ab60
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-02 18:45:29 -0600

    update

--- update-incident.php
+++ update-incident.php
@@ -16,7 +16,7 @@ session_start();
 
 
     if (isset($_POST['update'])) {
-        $id = $_POST['id'];
+        $id = (int)$_POST['id'];
         $inc_num = $_POST['inc_num'];
         $priority = $_POST['priority'];
         $description = $_POST['description'];
@@ -26,7 +26,7 @@ session_start();
         $time = $_POST['time'];
 
         // Update Statement
-        $update = "UPDATE incidents SET inc_num='$inc_num',priority='$priority',description='$description',assign_group='$assign_group',kb_article='$kb_article',date='$date',time='$time' WHERE id='1'";
+        $update = "UPDATE incidents SET inc_num='$inc_num',priority='$priority',description='$description',assign_group='$assign_group',kb_article='$kb_article',date='$date',time='$time' WHERE id='$id'";
         // if(count($_POST)>0) {
         //     mysqli_query($con,"UPDATE incidents set inc_num='" . $_POST['inc_num'] . "', priority='" . $_POST['priority'] . "', description='" . $_POST['description'] . "', assign_group='" . $_POST['assign_group'] . "' ,kb_article='" . $_POST['kb_article'] . "' ,date='" . $_POST['date'] . "' ,time='" . $_POST['time'] . "' where id='" . $_POST['id'] . "'");
         // $message = "Record Modified Successfully";
