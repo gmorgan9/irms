a28ccb51f628bcc784a946fd2b49c54f6709f2c2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 20:26:40 -0600

    update

--- incident-notes.php
+++ incident-notes.php
@@ -113,7 +113,7 @@ session_start();
             <th scope="row"><?php echo $id; ?></th>
             <td scope="row"><?php echo $date; ?></td>
             <td scope="row"><?php echo $title; ?></td>
-            <td style="position: absolute;color:black;max-width: 40em;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100px;"><?php echo $note; ?></td>
+            <td style="position: relative;color:black;max-width: 40em;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100px;"><?php echo $note; ?></td>
             <td scope="row"><?php echo $tag; ?></td>
             <td scope="row"><?php echo $time; ?></td>
             <td scope="row"><a href="view-note.php?viewid=<?php echo $id; ?>"><i class="fa-solid fa-eye" style="color:#1f7828;"></a></i></td>
