6beee1b5884ed9e9d1e784b4922a2b2009e0ebcd
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 20:40:56 -0600

    update

---
+++ view-note.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+    include("database/connection.php");
+    include("database/functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font Awesome -->
+    <link href="assets/fontawesome/css/all.css" rel="stylesheet">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/style.css?v=2.17">
+
+    <!-- Bootstrap Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- CKEDITOR -->
+    <script src="//cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
+    <script src="ckeditor/ckeditor.js"></script>
+
+    <title>View Note - IRMS</title>
+</head>
+<body>
+    <div class="header">
+        <h2 class="logo">
+            Incident Record Management System
+        </h2>
+    </div>
+
+
+<br><br>
+<div class="d-flex justify-content-center">
+    
+
+</div>
+
+</body>
+</html>
