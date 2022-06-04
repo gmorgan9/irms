b21acc8df9c44942a04fc5a28dbf4977d2db70aa
Garrett Morgan <gar.mor.pro@outlook.com>
2022-06-03 20:55:46 -0600

    update

--- view-note.php
+++ view-note.php
@@ -45,7 +45,7 @@ session_start();
             $note = $row['note'];
             $tag = $row['tag'];
           }
-          $newDate = date("M D Y", strtotime($date));
+          $newDate = date("M d, Y", strtotime($date));
         }
             ?>
 
